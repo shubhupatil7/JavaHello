703eed14e55b105eaa4dffba4e4469a55daa850e
shubhupatil7 <124869230+shubhupatil7@users.noreply.github.com>
2023-02-19 10:56:13 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Mr Patil"); 
-       System.out.println("Hello Mr Patil");  
-         System.out.println("Hello Mr Patil");  
+       System.out.println("This is Automation");  
+          
     }  
 }  
